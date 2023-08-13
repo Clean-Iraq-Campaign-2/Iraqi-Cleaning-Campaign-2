045b35f8e44b2c44e4835fc60928f137df82e059
wiaam-kareem <wiaamkareem93@gmail.com>
2023-08-13 07:36:55 +0300

    fix bugs

--- src/app/[locale]/homePageComponents/HeroSection.tsx
+++ src/app/[locale]/homePageComponents/HeroSection.tsx
@@ -6,7 +6,6 @@ import {useTranslations} from 'next-intl';
 import { useMediaQuery } from '@react-hook/media-query';
 import { useRouter } from "next/navigation";
 import { ROUTES } from "@/app/routes";
-import { Container } from "@/global/components/Container";
 
 export const HeroSection = () => {
   const t = useTranslations('Index');
@@ -18,15 +17,17 @@ export const HeroSection = () => {
   buttonText:"انضم ويانة"
 }
   return (
-    <section className=" relative w-screen h-96 md:h-[40rem] bg-gradient-to-b from-black to-transparent text-white text-end my-auto ">
-    <Image src={HeroImage} alt="" className=" absolute w-full h-full object-cover opacity-80"/>
-    <div className="absolute flex flex-col gap-2 items-end  px-4 md:px-40 top-0 bottom-0 my-auto h-fit right-0 text-end">
-       <div className="border py-1 md:py-2 px-2 md:px-8 border-white rounded-md rounded-tl-3xl w-fit">
+    <section className=" relative w-screen h-96 md:h-[40rem] text-white text-end my-auto ">
+    <Image src={HeroImage} alt="" className=" absolute w-full h-full object-cover"/>
+    <div className="absolute flex flex-col  w-full px-4 md:px-40   right-0 text-end  bg-black  bg-opacity-30 h-full">
+     <div className=" top-0 bottom-0 my-auto flex flex-col gap-2 items-end h-fit bg-opacity-100 text-opacity-100">
+         <div className="border py-1 md:py-2 px-2 md:px-8 border-white rounded-md rounded-tl-3xl w-fit">
        <h1 className="text-sm md:text-3xl ">{HERO_CONTENT.title}</h1>
        </div>
        <p className="text-xs md:text-lg ">{HERO_CONTENT.subTitle}</p>
          <MainButton text={HERO_CONTENT.buttonText} onClick={()=>{
           router.push(ROUTES.join)}}/>
+  </div>
     </div>
     </section>
   )
